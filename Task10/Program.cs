6c599e0618e61af1896ecae2505c6a3c5d164afa
Elena1011 <116499734+Elena1011@users.noreply.github.com>
2022-12-05 11:08:09 +0300

    Задачи

---
+++ Task10/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 10: Напишите программу, которая принимает 
+// на вход трёхзначное число и на выходе показывает вторую 
+// цифру этого числа. Выполнить с помощью числовых операций 
+// (целочисленное деление, остаток от деления).
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int function(int num)
+{
+    int num1 = num / 10;
+    int num2 = num1 % 10;
+    return num2;
+}
+
+bool IsTrue = true;
+while (IsTrue)
+{
+    Console.Write("Введите трехзначное число: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    if (number < 0) number = -number;
+    if (number < 1000 && number > 100)
+    {
+        int result = function(number);
+        Console.WriteLine($"Вторая цифра числа {result}");
+        IsTrue = false;
+    }
+}
---
+++ Task15/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 15: Напишите программу, которая принимает 
+// на вход цифру, обозначающую день недели, и проверяет, 
+// является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+
+Console.Write("Введите день недели: ");
+Console.Write("Введите день недели (1...7): ");
+int day = Convert.ToInt32(Console.ReadLine());
+
+bool Weekend(int week)
+{
+    return week == 6 || week == 7;
+}
+
+// Функция
+bool result = Weekend(day);
+Console.Write($"{day} -> ");
+Console.WriteLine(result ? "Да" : "Нет");
